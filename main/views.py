4be295cb5177fe23e40b0a5eff5dc66cb300d25a
MHibriziF <hibrizifarghana@gmail.com>
2024-09-04 14:14:11 +0700

    configure routings and fixed bugs in main.html

--- main/views.py
+++ main/views.py
@@ -3,8 +3,8 @@ from django.shortcuts import render
 def show_main(request):
     context = {
         'appname' : 'Easy Shop',
-        'name': 'Muhammad Hibrizi Farghana',
-        'class': 'PBP A'
+        'nama': 'Muhammad Hibrizi Farghana',
+        'kelas': 'PBP A'
     }
 
     return render(request, "main.html", context)
