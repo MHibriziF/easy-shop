351cbae3db8cd55c4d45b69e61b2f872ce13db4d
MHibriziF <hibrizifarghana@gmail.com>
2024-09-04 14:09:30 +0700

    add new views function and adjust main.html

--- main/views.py
+++ main/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
-# Create your views here.
+def show_main(request):
+    context = {
+        'appname' : 'Easy Shop',
+        'name': 'Muhammad Hibrizi Farghana',
+        'class': 'PBP A'
+    }
+
+    return render(request, "main.html", context)
