d10e6bfbb53e74021dd903e2fd87d75738d89451
MHibriziF <hibrizifarghana@gmail.com>
2024-09-16 14:01:45 +0700

    Add: create_product_entry function for product entries

--- main/views.py
+++ main/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from main.forms import ProductEntryForm
+from main.models import Product
 
 def show_main(request):
     context = {
@@ -8,3 +10,13 @@ def show_main(request):
     }
 
     return render(request, "main.html", context)
+
+def create_product_entry(request):
+    form = ProductEntryForm(request.POST or None)
+
+    if form.is_valid() and request.method == "POST":
+        form.save()
+        return redirect('main:show_main')
+
+    context = {'form': form}
+    return render(request, "create_product_entry.html", context)
