460a3924005843ef95bff709be33cd8b25e98ae3
MHibriziF <hibrizifarghana@gmail.com>
2024-09-21 13:40:09 +0700

    Create Register and Add forms

--- main/views.py
+++ main/views.py
@@ -1,6 +1,9 @@
 from django.http import HttpResponse
 from django.core import serializers
 from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import authenticate, login
+from django.contrib import messages
 from main.forms import ProductEntryForm
 from main.models import Product
 
@@ -14,6 +17,34 @@ def show_main(request):
     }
 
     return render(request, "main.html", context)
+
+def register(request):
+    form = UserCreationForm()
+
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Your account has been successfully created!')
+            return redirect('main:login')
+    context = {'form':form}
+
+    return render(request, 'register.html', context)
+
+def login_user(request):
+   if request.method == 'POST':
+      form = AuthenticationForm(data=request.POST)
+
+      if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('main:show_main')
+
+   else:
+      form = AuthenticationForm(request)
+      
+   context = {'form': form}
+   return render(request, 'login.html', context)
 
 def create_product_entry(request):
     form = ProductEntryForm(request.POST or None)
