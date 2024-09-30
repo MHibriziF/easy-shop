ccb3d99f7deeca3361360c65c8b322bc9166a8c2
MHibriziF <hibrizifarghana@gmail.com>
2024-09-30 13:52:03 +0700

    add: routes to edit

--- main/urls.py
+++ main/urls.py
@@ -12,5 +12,6 @@ urlpatterns = [
     path('json/<str:id>/', show_json_by_id, name='show_json_by_id'),
     path('register/', register, name="register"),
     path('login/', login_user, name='login'),
-    path('logout/', logout_user, name='logout')
+    path('logout/', logout_user, name='logout'),
+    path('edit-product/<uuid:id>', edit_product, name='edit_product'),
 ]
