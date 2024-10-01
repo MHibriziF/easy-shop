fe53e6435db85787d16dfb9ba40cb01f66a25795
MHibriziF <hibrizifarghana@gmail.com>
2024-10-01 08:57:07 +0700

    restyle login and delete redundant forms

--- main/forms.py
+++ main/forms.py
@@ -7,14 +7,3 @@ class ProductEntryForm(ModelForm):
     class Meta:
         model = Product
         fields = ["name", "price", "description", "stock"]
-
-class LoginForm(AuthenticationForm):
-    username = forms.CharField(widget=forms.TextInput(attrs={
-        'class': 'w-64 py-2 px-4 border border-gray-300 rounded-md',
-        'placeholder': 'Username'
-    }))
-
-    password = forms.CharField(widget=forms.PasswordInput(attrs={
-        'class': 'w-64 mt-2 py-2 px-4 border border-gray-300 rounded-md',
-        'placeholder': 'Password'
-    }))
