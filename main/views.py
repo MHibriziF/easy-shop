e3108c33d3971bd413c3246f325f860dd38a0154
MHibriziF <hibrizifarghana@gmail.com>
2024-10-07 13:33:07 +0700

    error message logging in

--- main/views.py
+++ main/views.py
@@ -37,21 +37,23 @@ def register(request):
     return render(request, 'register.html', context)
 
 def login_user(request):
-   if request.method == 'POST':
-      form = AuthenticationForm(data=request.POST)
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
 
-      if form.is_valid():
+        if form.is_valid():
             user = form.get_user()
             login(request, user)
             response = HttpResponseRedirect(reverse("main:show_main"))
             response.set_cookie('last_login', str(datetime.datetime.now()))
             return response
+        else:
+            messages.error(request, "Please try again.")
 
-   else:
-      form = AuthenticationForm(request)
+    else:
+        form = AuthenticationForm(request)
 
-   context = {'form': form}
-   return render(request, 'login.html', context)
+    context = {'form': form}
+    return render(request, 'login.html', context)
 
 def logout_user(request):
     logout(request)
