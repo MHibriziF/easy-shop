519aa908e9340c977e34927d414f64174a340a7c
MHibriziF <hibrizifarghana@gmail.com>
2024-12-02 13:56:08 +0700

    fix csrf

--- easy_shop/settings.py
+++ easy_shop/settings.py
@@ -131,3 +131,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+CSRF_TRUSTED_ORIGINS = ["http://localhost","http://127.0.0.1","http://muhammad-hibrizi-easyshop.pbp.cs.ui.ac.id/", "https://*.pbp.cs.ui.ac.id/"]
